f61cbe6812b270d441c6da9739fd9c961c8bbf3a
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2023-11-02 18:38:25 -0300

    projects

--- src/components/Swipper/Swipper.jsx
+++ src/components/Swipper/Swipper.jsx
@@ -1,24 +1,35 @@
 import styles from "./swipper.module.css";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      duration: 1000,
+
+      // Opciones de configuración de AOS (si es necesario)
+    });
+  }, []);
   const projects = [
     {
-      name: "Proyecto 1",
-      background: "/videogames2.jpg",
+      name: "Rick & Morty",
+      background: "/rickmorty.jpg",
       repoLink: "https://github.com/tuusuario/proyecto1",
       deployLink: "https://deploy1.com",
     },
     {
-      name: "Proyecto 1",
-      background: "/videogames2.jpg",
-      repoLink: "https://github.com/tuusuario/proyecto1",
-      deployLink: "https://deploy1.com",
-    },
-    {
-      name: "Videogames API",
+      name: "Videogames",
       background: "/videogames2.jpg",
       repoLink: "https://github.com/Matiasmarensi/VideoGames",
       deployLink: "https://video-games-git-main-matiasmarensi.vercel.app/",
+    },
+    {
+      name: "El Festín e-commerce",
+      background: "/elfestin.jpg",
+      repoLink: "https://github.com/Matiasmarensi/PF-Server",
+
+      deployLink: "https://pf-front-end-grupo3.vercel.app/home",
     },
     // Agrega más proyectos aquí
   ];
@@ -28,7 +39,7 @@ export default function App() {
       <h1>Projects</h1>
       <div className={styles.projectContainer}>
         {projects.map((project, index) => (
-          <div className={styles.projectCard} key={index}>
+          <div data-aos="flip-up" className={styles.projectCard} key={index}>
             <div className={styles.projectInfo}>
               <img src={project.background} alt={project.name} className={styles.projectImage} />
               <h5>{project.name}</h5>
