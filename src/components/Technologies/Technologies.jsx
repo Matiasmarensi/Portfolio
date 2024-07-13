39b0c826f74aae2bd6dc7d412ee835573dd143af
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-07-13 18:30:46 -0300

    marquee react slider

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -2,14 +2,15 @@ import styles from "./technologies.module.css";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Marquee from "react-fast-marquee";
+
 export default function Technologies() {
   const defaultWidth = 62;
+
   useEffect(() => {
     AOS.init({
       once: false,
       duration: 1000,
-
-      // Opciones de configuración de AOS (si es necesario)
     });
   }, []);
 
@@ -36,26 +37,26 @@ export default function Technologies() {
     <div id="technologies" className={styles.container}>
       <h1 data-aos="zoom-in">Technologies</h1>
       <div className={styles.box}>
-        <div className={styles.column} data-aos="flip-left">
-          <ul className={styles.rowContainer}>
+        <Marquee gradient={false} speed={40}>
+          <div className={styles.rowContainer}>
             {technologiesLeft.map((tech, index) => (
-              <li key={index} className={styles.row}>
+              <div key={index} className={styles.row} data-aos="flip-left">
                 <img className={styles.img} src={tech.icon} alt={tech.name} width={tech.width} height={tech.width} />
                 <p className={styles.pText}>{tech.name}</p>
-              </li>
+              </div>
             ))}
-          </ul>
-        </div>
-        <div className={styles.column} data-aos="flip-right">
-          <ul className={styles.rowContainer}>
+          </div>
+        </Marquee>
+        <Marquee gradient={false} speed={40} direction="right">
+          <div className={styles.rowContainer}>
             {technologiesRight.map((tech, index) => (
-              <li key={index} className={styles.row}>
+              <div key={index} className={styles.row} data-aos="flip-right">
                 <img className={styles.img} src={tech.icon} alt={tech.name} width={tech.width} height={tech.width} />
                 <p className={styles.pText}>{tech.name}</p>
-              </li>
+              </div>
             ))}
-          </ul>
-        </div>
+          </div>
+        </Marquee>
       </div>
     </div>
   );
