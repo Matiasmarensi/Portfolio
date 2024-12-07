1918e5f0840c74eb83f74481456c8179a929bf7b
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-12-06 21:34:48 -0300

    hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BaseInfo } from "@/Data/data";
-import { FaDownload, FaDownLong } from "react-icons/fa6";
+import { FaDownload } from "react-icons/fa6";
 import Image from "next/image";
 
 const Hero = () => {
