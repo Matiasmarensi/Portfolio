e154bc3d1907972909b1118819cad0c95849e6ae
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-12-09 21:24:32 -0300

    projects 1

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
+import Project from "./Project/Project";
 
 const Home = () => {
   return (
     <div className="overflow-hidden bg-slate-700 ">
       <Hero />
       <About />
+      <Project />
     </div>
   );
 };
