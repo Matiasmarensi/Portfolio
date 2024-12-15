06e6997045eec72bbec2b2a8c6db93886aaa9321
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-12-15 19:41:44 -0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,29 @@ const font = Electrolize({
 });
 export const metadata: Metadata = {
   title: "Matias Marensi Fullstack",
-  description: "Webdev porfolio",
+  description: "Portfolio personal de Matias Marensi como Fullstack Developer.",
+  openGraph: {
+    title: "Matias Marensi Fullstack",
+    description: "Explora mi portfolio y proyectos como Fullstack Developer.",
+    url: "https://portfolio-matiasmarensi.vercel.app/", // Reemplaza con tu URL
+    siteName: "Matias Marensi Portfolio",
+    images: [
+      {
+        url: "/images/portfolio.jpg", // Ruta a tu imagen Open Graph (idealmente 1200x630 px)
+        width: 1200,
+        height: 630,
+        alt: "Vista previa del portfolio de Matias Marensi",
+      },
+    ],
+    locale: "es_ES", // Cambia según tu idioma y región, ej: en_US, es_ES
+    type: "website", // Define el tipo del sitio (website, article, etc.)
+  },
+  twitter: {
+    card: "summary_large_image", // Define el tipo de tarjeta para Twitter
+    title: "Matias Marensi Fullstack",
+    description: "Portfolio personal de Matias Marensi como Fullstack Developer.",
+    images: ["/images/portfolio.jpg"], // Ruta a tu imagen Open Graph
+  },
 };
 
 export default function RootLayout({
