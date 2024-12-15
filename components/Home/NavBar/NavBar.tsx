96719b6707383b24cbd152f9bad99bf98a79db0d
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-12-15 19:34:28 -0300

    import no usados

--- components/Home/NavBar/NavBar.tsx
+++ components/Home/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { navLinks } from "../../../constant/constant";
